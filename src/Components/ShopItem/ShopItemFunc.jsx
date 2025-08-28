ad252aba3320edfade6ffee372c231100ede4d41
Patience-labour <maxonklaxon522@gmail.com>
2025-08-28 15:04:49 +0300

    Complete react

---
+++ src/Components/ShopItem/ShopItemFunc.jsx
@@ -0,0 +1,23 @@
+export const ShopItemFunc = ({ item }) => {
+
+    const formatPrice = `${item.currency}${item.price.toFixed(2)}`;
+
+    return (
+        <div className="main-content">
+            <h2>{item.brand}</h2>
+            <h1>{item.title}</h1>
+            <h3>{item.description}</h3>
+            <div className="description">
+                {item.descriptionFull}
+            </div>
+            <div className="highlight-window mobile">
+                <div className="highlight-overlay"></div>
+            </div>
+            <div className="divider"></div>
+            <div className="purchase-info">
+                <div className="price">{formatPrice}</div>
+                <button>Добавить в корзину</button>
+            </div>
+        </div>
+    );
+}
